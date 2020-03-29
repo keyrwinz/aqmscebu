5c3348a0b2a09083a7a8393426b9550a1ef8444e
keyrwinz <keyrwinfelisilda@gmail.com>
2020-03-29 21:04:29 +0800

    added google analytics

--- src/firebase.js
+++ src/firebase.js
@@ -7,12 +7,14 @@ const firebaseConfig = {
   projectId: "aqms-db-thesis-2020",
   storageBucket: "aqms-db-thesis-2020.appspot.com",
   messagingSenderId: "1089415169318",
-  appId: "1:1089415169318:web:851e60a6b53bff02ea6de7"
+  appId: "1:1089415169318:web:851e60a6b53bff02ea6de7",
+  measurementId: "G-Y2P4T33T38",
 };
 
 if (typeof window !== 'undefined'){
   firebase = require('firebase');
   firebase.initializeApp(firebaseConfig);
+  firebase.analytics()
 }
 
 export default firebase;
