c4a0050e2e78edd70c70f7d44a14f2874aea6494
keyrwinz <keyrwinfelisilda@gmail.com>
2020-04-02 21:10:27 +0800

    adding firebase realtime db sdk

--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -1,15 +1,28 @@
 let firebase;
 
+// CONFIG FOR REALTIME DB
 const firebaseConfig = {
-  apiKey: process.env.API_FIRESTORE,
-  authDomain: "aqms-db-thesis-2020.firebaseapp.com",
-  databaseURL: "https://aqms-db-thesis-2020.firebaseio.com",
-  projectId: "aqms-db-thesis-2020",
-  storageBucket: "aqms-db-thesis-2020.appspot.com",
-  messagingSenderId: "1089415169318",
-  appId: "1:1089415169318:web:851e60a6b53bff02ea6de7",
-  measurementId: "G-Y2P4T33T38",
+  apiKey: process.env.API_FIREBASE,
+  authDomain: "aqmscebu.firebaseapp.com",
+  databaseURL: "https://aqmscebu.firebaseio.com",
+  projectId: "aqmscebu",
+  storageBucket: "aqmscebu.appspot.com",
+  messagingSenderId: "87641127223",
+  appId: "1:87641127223:web:1e3c9b20b0176b221202b2",
+  measurementId: "G-SYM7TXDQB0"
 };
+
+// CONFIG FOR FIRESTORE
+// const firebaseConfig = {
+//   apiKey: process.env.API_FIRESTORE,
+//   authDomain: "aqms-db-thesis-2020.firebaseapp.com",
+//   databaseURL: "https://aqms-db-thesis-2020.firebaseio.com",
+//   projectId: "aqms-db-thesis-2020",
+//   storageBucket: "aqms-db-thesis-2020.appspot.com",
+//   messagingSenderId: "1089415169318",
+//   appId: "1:1089415169318:web:851e60a6b53bff02ea6de7",
+//   measurementId: "G-Y2P4T33T38",
+// };
 
 if (typeof window !== 'undefined'){
   firebase = require('firebase');
--- src/pages/download-data.js
+++ src/pages/download-data.js
@@ -4,12 +4,34 @@ import Layout from '../components/layout'
 import Firebase from '../components/Firebase/firebase'
 import RenderData from '../components/RenderData'
 import SEO from '../components/seo'
+import FirestoreData from '../../TEMP_FOLDER/uscmc.json'
 
 const DownloadData = () => {
   const [data, setData] = useState([])
   const [node, setNode] = useState('usc-mc')
 
   // const addtemp = 10
+  const firebaseDB = Firebase.database()
+  const nodeRef = firebaseDB.ref('aqmnodes/usc-mc/states')
+
+  const addData = () => {
+    FirestoreData.map((d) => {
+      nodeRef.push(d)
+    })
+    console.log('uploaded!')
+  }
+
+  useEffect(() => {
+    // const starCountRef = firebaseDB.ref('usc-mc/states')
+    //   .on('value', (snapshot) => {
+    //     console.log(snapshot.val())
+    //   })
+
+    // return () => {
+    //   starCountRef.off()
+    // }
+  }, [])
+
 
   // const addData = async () => {
   //   const db = await firebase.firestore()
@@ -35,26 +57,25 @@ const DownloadData = () => {
   //     })
   // }
 
-  useEffect(() => {
-    console.log('fetching data...')
-    const unsubscribe = Firebase
-      .firestore()
-      .collection('aqms-cebu')
-      .doc(node)
-      .collection('states')
-      .orderBy('timestamp')
-      .onSnapshot((snapshot) => {
-        const newData = []
-        snapshot.docs.forEach((d) => {
-          newData.push(d.data())
-        })
-        setData(newData.sort((a, b) => b.timestamp - a.timestamp))
-      })
-
-    return () => {
-      unsubscribe()
-    }
-  }, [node, setData])
+  // useEffect(() => {
+  //   console.log('fetching data...')
+  //   const unsubscribe = Firebase
+  //     .firestore()
+  //     .collection('aqms-cebu')
+  //     .doc(node)
+  //     .collection('states')
+  //     .orderBy('timestamp')
+  //     .onSnapshot((snapshot) => {
+  //       const newData = []
+  //       snapshot.docs.forEach((d) => {
+  //         newData.push(d.data())
+  //       })
+  //       setData(newData.sort((a, b) => b.timestamp - a.timestamp))
+  //     })
+  //   return () => {
+  //     unsubscribe()
+  //   }
+  // }, [node, setData])
 
   let renderComponent = null
   if (node === 'usc-mc') {
@@ -86,7 +107,7 @@ const DownloadData = () => {
                   <option>usc-sc</option>
                 </select>
               </div>
-              {/* <button className="floatRight" onClick={() => addData(node)}>Add data</button> */}
+              <button className="floatRight" type="button" onClick={() => addData(node)}>Add data</button>
             </div>
           </div>
           <div className="row">
