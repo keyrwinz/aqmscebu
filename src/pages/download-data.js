137159b69cada7d891c752f77f5db4759ef49c15
keyrwinz <keyrwinfelisilda@gmail.com>
2020-04-27 20:38:44 +0800

    added initial pagination for data

--- src/pages/download-data.js
+++ src/pages/download-data.js
@@ -1,129 +1,151 @@
+/* eslint-disable jsx-a11y/label-has-associated-control */
 import React, { useState, useEffect } from 'react'
+import { useStaticQuery, graphql } from 'gatsby'
 import styled from 'styled-components'
-import Layout from '../components/layout'
+import moment from 'moment'
+import FirebasePaginator from 'firebase-paginator'
 import Firebase from '../components/Firebase/firebase'
+import Nodes from '../components/GoogleMap/AqmsNodes'
+import Layout from '../components/layout'
+import Color from '../components/Theme/ColorPallete'
 import RenderData from '../components/RenderData'
 import SEO from '../components/seo'
-import FirestoreData from '../../TEMP_FOLDER/uscmc.json'
+// import FirestoreData from '../../TEMP_FOLDER/uscsc.json'
 
 const DownloadData = () => {
+  const author = useStaticQuery(graphql`
+    query AuthorQuery {
+      site {
+        siteMetadata {
+          author
+        }
+      }
+    }
+  `)
+  console.log('RENDERED!')
+
   const [data, setData] = useState([])
   const [node, setNode] = useState('usc-mc')
+  const [pageSize, setPageSize] = useState(5)
+  const [paginatorInstance, setPaginator] = useState(null)
 
-  // const addtemp = 10
-  const firebaseDB = Firebase.database()
-  const nodeRef = firebaseDB.ref('aqmnodes/usc-mc/states')
-
-  const addData = () => {
-    FirestoreData.map((d) => {
-      nodeRef.push(d)
-    })
-    console.log('uploaded!')
+  const options = {
+    pageSize,
+    finite: false,
+    auth: process.env.API_FIREBASE,
+    retainLastPage: false,
   }
 
   useEffect(() => {
-    // const starCountRef = firebaseDB.ref('usc-mc/states')
-    //   .on('value', (snapshot) => {
-    //     console.log(snapshot.val())
-    //   })
+    const firebaseDB = Firebase.database()
+    const nodeRef = firebaseDB.ref(`aqmnodes/${node}/states`)
+    const paginator = new FirebasePaginator(nodeRef, options)
+    setPaginator(paginator)
 
-    // return () => {
-    //   starCountRef.off()
-    // }
-  }, [])
+    const firebaseCallback = () => {
+      const { collection } = paginator
+      if (Object.keys(collection).length !== 0) {
+        const entries = Object.entries(collection).reverse()
+        const response = entries.map((d) => d[1])
+        setData(response)
+      }
+    }
 
+    paginator.on('value', firebaseCallback)
 
-  // const addData = async () => {
-  //   const db = await firebase.firestore()
-  //   const time = new Date()
-  //   db.collection('aqms-cebu')
-  //     .doc(node)
-  //     .collection('states')
-  //     .doc()
-  //     .set({
-  //       humidity: 100,
-  //       no2: addtemp + 1,
-  //       so2: addtemp + 2,
-  //       pm10: addtemp + 3,
-  //       pm25: addtemp + 4,
-  //       temp: addtemp + 5,
-  //       timestamp: time.getTime(),
-  //     })
-  //     .then(() => {
-  //       console.log('Document successfully written!')
-  //     })
-  //     .catch((error) => {
-  //       console.error('Error writing document: ', error)
-  //     })
-  // }
+    paginator.listen((eventName, eventPayload) => {
+      console.log(`Fired ${eventName} with the following payload: `, eventPayload)
+    })
 
-  // useEffect(() => {
-  //   console.log('fetching data...')
-  //   const unsubscribe = Firebase
-  //     .firestore()
-  //     .collection('aqms-cebu')
-  //     .doc(node)
-  //     .collection('states')
-  //     .orderBy('timestamp')
-  //     .onSnapshot((snapshot) => {
-  //       const newData = []
-  //       snapshot.docs.forEach((d) => {
-  //         newData.push(d.data())
-  //       })
-  //       setData(newData.sort((a, b) => b.timestamp - a.timestamp))
-  //     })
-  //   return () => {
-  //     unsubscribe()
-  //   }
-  // }, [node, setData])
+    return () => {
+      paginator.off('value', firebaseCallback)
+    }
+  }, [node])
 
-  let renderComponent = null
-  if (node === 'usc-mc') {
-    renderComponent = <RenderData data={data} />
-  } else if (node === 'usc-sc') {
-    renderComponent = <RenderData data={data} />
+  const onChangeHandler = (value) => setNode(value)
+  const onPrevHandler = () => paginatorInstance.previous()
+  const onNextHandler = () => paginatorInstance.next()
+
+  let isLastPage
+  if (paginatorInstance) {
+    isLastPage = paginatorInstance.isLastPage || false
   }
-
-  const onChangeHandler = (e) => {
-    setNode(e.target.value)
-  }
-
+  console.log({ isLastPage })
   return (
     <Layout>
       <SEO title="Download Data" />
       <Style>
         <div className="container-sm">
           <div className="row">
-            <div className="col statesHeader">
-              <div className="form-group">
-                {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
-                <label>Select Node</label>
-                <select
-                  className="form-control customSelect"
-                  onChange={(e) => onChangeHandler(e)}
-                  value={node}
-                >
-                  <option>usc-mc</option>
-                  <option>usc-sc</option>
-                </select>
+            <div className="d-flex bd-highlight w-100">
+              <div className="p-2 flex-grow-1 bd-highlight">
+                <div className="input-group w-25">
+                  <div className="input-group-prepend">
+                    <label className="input-group-text" htmlFor="nodeSelection">Node</label>
+                  </div>
+                  <select
+                    className="custom-select"
+                    id="nodeSelection"
+                    onChange={(e) => onChangeHandler(e.target.value)}
+                    value={node}
+                  >
+                    { Nodes.nodesLoc.map((loc, index) => (
+                      <option
+                        // eslint-disable-next-line react/no-array-index-key
+                        key={index}
+                        value={loc.id}
+                      >
+                        {loc.text}
+                      </option>
+                    )) }
+                  </select>
+                </div>
               </div>
-              <button className="floatRight" type="button" onClick={() => addData(node)}>Add data</button>
+              <div className="p-2 bd-highlight">
+                <ul className="pagination">
+                  <li className="page-item">
+                    <button
+                      className="page-link"
+                      type="button"
+                      aria-label="Previous"
+                      onClick={() => onNextHandler()}
+                    >
+                      <span aria-hidden="true">&laquo;</span>
+                    </button>
+                  </li>
+                </ul>
+              </div>
+              <div className="p-2 bd-highlight">
+                <ul className="pagination">
+                  <li className={`page-item ${isLastPage ? 'disabled' : ''}`}>
+                    <button
+                      style={{ background: `${isLastPage ? 'black' : ''}` }}
+                      className="page-link"
+                      type="button"
+                      aria-label="Next"
+                      onClick={() => onPrevHandler()}
+                    >
+                      <span aria-hidden="true">&raquo;</span>
+                    </button>
+                  </li>
+                </ul>
+              </div>
             </div>
           </div>
-          <div className="row">
-            <div className="col" style={{ color: 'black' }}>
-              {renderComponent}
+          <div className="row" style={{ marginRight: '-10px' }}>
+            <div className="col" style={{ color: 'black', overflowX: 'auto' }}>
+              <RenderData data={data} pageSize={pageSize} />
             </div>
           </div>
           <div className="row">
             <div className="col">
-              <div className="footer page-footer borderbox">
+              <div className="footer borderbox" style={{ color: 'white' }}>
                 ©
-                {' '}
-                {new Date().getFullYear()}
+                { new Date().getFullYear() }
                 , Built by
-                <a href="#top">
-                  WAYDSB Thesis2020
+                &nbsp;
+                <a href="#download-data">
+                  {author.site.siteMetadata.author}
                 </a>
               </div>
             </div>
@@ -137,6 +159,20 @@ const DownloadData = () => {
 const Style = styled.div`
   .customSelect {
     width: 200px;
+  }
+  .footer {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background: ${Color.thirdColor};
+    min-height: 91px;
+  }
+
+  .borderbox {
+    border-radius: 3px;
+    border: 1px solid ${Color.fourthColor}33;
+    background: ${Color.thirdColor};
+    padding: 10px;
   }
 `
 
