5a893957872655a55ffbc10d5c52649d4f9a1d96
keyrwinz <keyrwinfelisilda@gmail.com>
2020-06-10 12:03:09 +0800

    updated google map

--- src/components/GoogleMap/GoogleMap.js
+++ src/components/GoogleMap/GoogleMap.js
@@ -2,145 +2,8 @@ import React, { useState } from 'react'
 import PropTypes from 'prop-types'
 import { GoogleMap, Marker, InfoWindow } from 'react-google-maps'
 import nodes from './AqmsNodes'
+import mapStyles from './GoogleMapStyle'
 import nodeIcon from '../../assets/images/NodeIcon.png'
-
-const mapStyles = [
-  {
-    featureType: 'all',
-    elementType: 'geometry',
-    stylers: [
-      {
-        color: '#202c3e',
-      },
-    ],
-  },
-  {
-    featureType: 'all',
-    elementType: 'labels.text.fill',
-    stylers: [
-      {
-        gamma: 0.01,
-      },
-      {
-        lightness: 20,
-      },
-      {
-        weight: '1.39',
-      },
-      {
-        color: '#ffffff',
-      },
-    ],
-  },
-  {
-    featureType: 'all',
-    elementType: 'labels.text.stroke',
-    stylers: [
-      {
-        weight: '0.96',
-      },
-      {
-        saturation: '9',
-      },
-      {
-        visibility: 'on',
-      },
-      {
-        color: '#000000',
-      },
-    ],
-  },
-  {
-    featureType: 'all',
-    elementType: 'labels.icon',
-    stylers: [
-      {
-        visibility: 'off',
-      },
-    ],
-  },
-  {
-    featureType: 'landscape',
-    elementType: 'geometry',
-    stylers: [
-      {
-        lightness: 30,
-      },
-      {
-        saturation: '9',
-      },
-      {
-        color: '#29446b',
-      },
-    ],
-  },
-  {
-    featureType: 'poi',
-    elementType: 'geometry',
-    stylers: [
-      {
-        saturation: 20,
-      },
-    ],
-  },
-  {
-    featureType: 'poi.park',
-    elementType: 'geometry',
-    stylers: [
-      {
-        lightness: 20,
-      },
-      {
-        saturation: -20,
-      },
-    ],
-  },
-  {
-    featureType: 'road',
-    elementType: 'geometry',
-    stylers: [
-      {
-        lightness: 10,
-      },
-      {
-        saturation: -30,
-      },
-    ],
-  },
-  {
-    featureType: 'road',
-    elementType: 'geometry.fill',
-    stylers: [
-      {
-        color: '#193a55',
-      },
-    ],
-  },
-  {
-    featureType: 'road',
-    elementType: 'geometry.stroke',
-    stylers: [
-      {
-        saturation: 25,
-      },
-      {
-        lightness: 25,
-      },
-      {
-        weight: '0.01',
-      },
-    ],
-  },
-  {
-    featureType: 'water',
-    elementType: 'all',
-    stylers: [
-      {
-        lightness: -20,
-      },
-    ],
-  },
-]
 
 const Map = ({ nodeSelectFunc }) => {
   const [selectedNode, setSelectedNode] = useState(null)
@@ -149,7 +12,13 @@ const Map = ({ nodeSelectFunc }) => {
     <GoogleMap
       defaultZoom={nodes.zoom}
       defaultCenter={nodes.center}
-      defaultOptions={{ styles: mapStyles }}
+      defaultOptions={
+        {
+          styles: mapStyles,
+          mapTypeControl: false,
+          streetViewControl: false,
+        }
+      }
     >
       {nodes.nodesLoc.map((node) => (
         <Marker
---
+++ src/components/GoogleMap/GoogleMapStyle.js
@@ -0,0 +1,368 @@
+export default [
+  {
+    featureType: 'all',
+    elementType: 'labels.text.fill',
+    stylers: [
+      {
+        saturation: 36,
+      },
+      {
+        color: '#000000',
+      },
+      {
+        lightness: 40,
+      },
+    ],
+  },
+  {
+    featureType: 'all',
+    elementType: 'labels.text.stroke',
+    stylers: [
+      {
+        visibility: 'on',
+      },
+      {
+        color: '#000000',
+      },
+      {
+        lightness: 16,
+      },
+    ],
+  },
+  {
+    featureType: 'all',
+    elementType: 'labels.icon',
+    stylers: [
+      {
+        visibility: 'off',
+      },
+    ],
+  },
+  {
+    featureType: 'administrative',
+    elementType: 'geometry.fill',
+    stylers: [
+      {
+        color: '#000000',
+      },
+      {
+        lightness: 20,
+      },
+    ],
+  },
+  {
+    featureType: 'administrative',
+    elementType: 'geometry.stroke',
+    stylers: [
+      {
+        color: '#000000',
+      },
+      {
+        lightness: 17,
+      },
+      {
+        weight: 1.2,
+      },
+    ],
+  },
+  {
+    featureType: 'administrative.country',
+    elementType: 'geometry.fill',
+    stylers: [
+      {
+        saturation: '97',
+      },
+      {
+        visibility: 'off',
+      },
+    ],
+  },
+  {
+    featureType: 'administrative.province',
+    elementType: 'labels',
+    stylers: [
+      {
+        color: '#9e9e9e',
+      },
+    ],
+  },
+  {
+    featureType: 'administrative.locality',
+    elementType: 'labels',
+    stylers: [
+      {
+        color: '#cecece',
+      },
+      {
+        visibility: 'simplified',
+      },
+    ],
+  },
+  {
+    featureType: 'administrative.neighborhood',
+    elementType: 'labels',
+    stylers: [
+      {
+        visibility: 'on',
+      },
+      {
+        saturation: '-62',
+      },
+      {
+        gamma: '1.10',
+      },
+    ],
+  },
+  {
+    featureType: 'administrative.land_parcel',
+    elementType: 'labels',
+    stylers: [
+      {
+        visibility: 'off',
+      },
+    ],
+  },
+  {
+    featureType: 'landscape',
+    elementType: 'geometry',
+    stylers: [
+      {
+        color: '#1a202c',
+      },
+      {
+        lightness: '-10',
+      },
+      {
+        gamma: '1.00',
+      },
+      {
+        saturation: '0',
+      },
+    ],
+  },
+  {
+    featureType: 'landscape.man_made',
+    elementType: 'labels.text.fill',
+    stylers: [
+      {
+        visibility: 'off',
+      },
+      {
+        saturation: '75',
+      },
+    ],
+  },
+  {
+    featureType: 'landscape.man_made',
+    elementType: 'labels.icon',
+    stylers: [
+      {
+        saturation: '8',
+      },
+    ],
+  },
+  {
+    featureType: 'landscape.natural.landcover',
+    elementType: 'all',
+    stylers: [
+      {
+        color: '#511818',
+      },
+      {
+        visibility: 'off',
+      },
+    ],
+  },
+  {
+    featureType: 'landscape.natural.landcover',
+    elementType: 'geometry',
+    stylers: [
+      {
+        visibility: 'off',
+      },
+      {
+        hue: '#c200ff',
+      },
+    ],
+  },
+  {
+    featureType: 'landscape.natural.terrain',
+    elementType: 'all',
+    stylers: [
+      {
+        visibility: 'on',
+      },
+    ],
+  },
+  {
+    featureType: 'poi',
+    elementType: 'geometry',
+    stylers: [
+      {
+        color: '#000000',
+      },
+      {
+        lightness: 21,
+      },
+    ],
+  },
+  {
+    featureType: 'road.highway',
+    elementType: 'geometry',
+    stylers: [
+      {
+        color: '#cf0f0f',
+      },
+    ],
+  },
+  {
+    featureType: 'road.highway',
+    elementType: 'geometry.fill',
+    stylers: [
+      {
+        color: '#000000',
+      },
+      {
+        lightness: 17,
+      },
+    ],
+  },
+  {
+    featureType: 'road.highway',
+    elementType: 'geometry.stroke',
+    stylers: [
+      {
+        color: '#000000',
+      },
+      {
+        lightness: 29,
+      },
+      {
+        weight: 0.2,
+      },
+    ],
+  },
+  {
+    featureType: 'road.highway',
+    elementType: 'labels',
+    stylers: [
+      {
+        color: '#b3adad',
+      },
+    ],
+  },
+  {
+    featureType: 'road.highway',
+    elementType: 'labels.text',
+    stylers: [
+      {
+        visibility: 'off',
+      },
+    ],
+  },
+  {
+    featureType: 'road.arterial',
+    elementType: 'geometry',
+    stylers: [
+      {
+        color: '#464646',
+      },
+      {
+        lightness: 18,
+      },
+    ],
+  },
+  {
+    featureType: 'road.local',
+    elementType: 'geometry',
+    stylers: [
+      {
+        color: '#000000',
+      },
+      {
+        lightness: 16,
+      },
+      {
+        visibility: 'on',
+      },
+    ],
+  },
+  {
+    featureType: 'transit',
+    elementType: 'geometry',
+    stylers: [
+      {
+        color: '#000000',
+      },
+      {
+        lightness: 19,
+      },
+    ],
+  },
+  {
+    featureType: 'transit.line',
+    elementType: 'geometry',
+    stylers: [
+      {
+        visibility: 'off',
+      },
+    ],
+  },
+  {
+    featureType: 'transit.line',
+    elementType: 'labels',
+    stylers: [
+      {
+        visibility: 'off',
+      },
+    ],
+  },
+  {
+    featureType: 'transit.station.airport',
+    elementType: 'labels',
+    stylers: [
+      {
+        visibility: 'on',
+      },
+    ],
+  },
+  {
+    featureType: 'transit.station.bus',
+    elementType: 'geometry',
+    stylers: [
+      {
+        visibility: 'off',
+      },
+    ],
+  },
+  {
+    featureType: 'transit.station.bus',
+    elementType: 'labels',
+    stylers: [
+      {
+        visibility: 'off',
+      },
+    ],
+  },
+  {
+    featureType: 'water',
+    elementType: 'geometry',
+    stylers: [
+      {
+        lightness: 17,
+      },
+      {
+        color: '#2c5282',
+      },
+    ],
+  },
+  {
+    featureType: 'water',
+    elementType: 'labels.text.fill',
+    stylers: [
+      {
+        lightness: '0',
+      },
+    ],
+  },
+]
