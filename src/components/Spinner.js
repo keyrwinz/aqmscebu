5cb4c769ec4da41180758d278a7998a542d971b7
keyrwinz <keyrwinfelisilda@gmail.com>
2020-06-11 20:36:32 +0800

    spinner updates

--- src/components/Spinner.js
+++ src/components/Spinner.js
@@ -1,21 +1,36 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import GrowingSpinner from 'react-bootstrap/Spinner'
+import Color from './Theme/ColorPallete'
 
-const Spinner = ({ small }) => {
-  let Style = {}
-  let customSpinner = {}
+/**
+ *
+ * @param { boolean } small -> Changes spinner size to 1rem, default size: 4rem
+ * @param { object } style -> To customize spinner container style
+ */
+const Spinner = ({ small, style }) => {
+  let Container = {
+    ...style,
+    color: Color.secondaryColor,
+  }
+  let CustomSpinner = {
+    width: '4rem',
+    height: '4rem',
+  }
 
   if (small) {
-    Style = {
+    Container = {
+      ...Container,
       display: 'inline-block',
     }
-    customSpinner = {
+    CustomSpinner = {
       width: '1rem',
       height: '1rem',
-      borderWidth: '0.15em',
     }
   } else {
-    Style = {
+    Container = {
+      ...Container,
+      height: '100%',
       minHeight: '100px',
       display: 'flex',
       alignItems: 'center',
@@ -24,20 +39,20 @@ const Spinner = ({ small }) => {
   }
 
   return (
-    <div style={Style}>
-      <div className="spinner-border text-warning" role="status" style={customSpinner}>
-        <span className="sr-only">Loading...</span>
-      </div>
+    <div style={Container}>
+      <GrowingSpinner style={CustomSpinner} animation="grow" />
     </div>
   )
 }
 
 Spinner.propTypes = {
   small: PropTypes.bool,
+  style: PropTypes.shape(),
 }
 
 Spinner.defaultProps = {
   small: false,
+  style: {},
 }
 
 
