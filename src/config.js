a6a362ef8343fa5d45ec63d4d6af512d4370170d
keyrwinz <keyrwinfelisilda@gmail.com>
2020-06-13 21:48:09 +0800

    update config

--- src/config.js
+++ src/config.js
@@ -10,15 +10,16 @@ export const AirMonitoringNodes = {
   center: { lat: 10.3157, lng: 123.8854 },
   zoom: 13,
   nodesLoc: [
+    /**
+     * @param { string } id -> Used to display selected node in UI
+     * @param { string } text -> Used to display as suggested text in search bar
+     * @param { number } lat_and_lng -> Latitude and Longitude
+     */
     {
-      id: 'usc-mc', lat: 10.299974, lng: 123.898434, text: 'USC MC NODE',
+      id: 'usc-mc', lat: 10.299974, lng: 123.898434, text: 'University of San Carlos, Main Campus', locality: 'Cebu City',
     },
     {
-      id: 'usc-sc', lat: 10.300467, lng: 123.887958, text: 'USC SC NODE',
+      id: 'usc-sc', lat: 10.300467, lng: 123.887958, text: 'University of San Carlos, South Campus', locality: 'Cebu City',
     },
-    // {
-    //   // [NODE_ID: string], [latitude: number], [longitude: number], [text: string]
-    //   id: 'NODE_ID', lat: <latitude>, lng: <longitude>, text: 'TEXT TO DISPLAY FOR THIS NODE'
-    // },
   ],
 }
