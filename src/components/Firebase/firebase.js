6fdd17a8ea668419cc2d853a56beb87089a33315
keyrwinz <keyrwinfelisilda@gmail.com>
2020-06-15 11:18:50 +0800

    update firebase config

--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -1,4 +1,7 @@
-let firebase
+import firebase from 'firebase/app'
+import 'firebase/auth'
+import 'firebase/database'
+import 'firebase/analytics'
 
 // CONFIG FOR REALTIME DB
 const firebaseConfig = {
@@ -12,23 +15,7 @@ const firebaseConfig = {
   measurementId: 'G-SYM7TXDQB0',
 }
 
-// CONFIG FOR FIRESTORE
-// const firebaseConfig = {
-//   apiKey: process.env.API_FIRESTORE,
-//   authDomain: "aqms-db-thesis-2020.firebaseapp.com",
-//   databaseURL: "https://aqms-db-thesis-2020.firebaseio.com",
-//   projectId: "aqms-db-thesis-2020",
-//   storageBucket: "aqms-db-thesis-2020.appspot.com",
-//   messagingSenderId: "1089415169318",
-//   appId: "1:1089415169318:web:851e60a6b53bff02ea6de7",
-//   measurementId: "G-Y2P4T33T38",
-// };
-
-if (typeof window !== 'undefined') {
-  // eslint-disable-next-line global-require
-  firebase = require('firebase')
-  firebase.initializeApp(firebaseConfig)
-  firebase.analytics()
-}
+firebase.initializeApp(firebaseConfig)
+firebase.analytics()
 
 export default firebase
