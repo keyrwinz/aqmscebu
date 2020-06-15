31f144188b2586aef3aaa4814140be5edc29fdc0
keyrwinz <keyrwinfelisilda@gmail.com>
2020-06-15 11:29:37 +0800

    update layout (refactored)

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,51 +1,34 @@
-/**
- * Layout component that queries for data
- * with Gatsby's useStaticQuery component
- *
- * See: https://www.gatsbyjs.org/docs/use-static-query/
- */
-
 import React from 'react'
 import PropTypes from 'prop-types'
-import { useStaticQuery, graphql } from 'gatsby'
-import Header from './header'
+import styled from 'styled-components'
+import Header from './Header'
+import Footer from './Footer'
 import Color from './Theme/ColorPallete'
 import './layout.css'
 import '../assets/css/bootstrap.css'
 
-const Layout = ({ children }) => {
-  const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
-        }
-      }
-    }
-  `)
-
-  return (
-    <>
-      <Header siteTitle={data.site.siteMetadata.title} />
-      <div
-        style={{
-          boxSizing: 'border-box',
-          background: Color.primaryColor,
-          color: Color.secondaryColor,
-          fontFamily: 'Roboto, sans-serif',
-          paddingTop: '15px',
-          paddingBottom: '12px',
-          minHeight: '100vh',
-        }}
-      >
-        <main>{children}</main>
-      </div>
-    </>
-  )
-}
+const Layout = ({ children }) => (
+  <>
+    <Header />
+    <MainStyle>
+      <main>{children}</main>
+    </MainStyle>
+    <Footer />
+  </>
+)
 
 Layout.propTypes = {
   children: PropTypes.node.isRequired,
 }
+
+const MainStyle = styled.div`
+  box-sizing: border-box;
+  background: ${Color.primaryColor};
+  color: ${Color.secondaryColor};
+  font-family: Roboto, sans-serif;
+  padding-top: 15px;
+  padding-bottom: 12px;
+  min-height: 100vh;
+`
 
 export default Layout
